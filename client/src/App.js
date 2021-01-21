3667b4fce75e8a5f39696527a809a4e334bd96fc
Anjali Kabariya <anjali.kabariya21@gmail.com>
2021-01-20 22:21:31 -0500

    routes working for navbar
    created pages and component folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react'
-import Axios from 'axios';
-import {SearchForm} from './components'
-const API_URL = "https://finnhub.io/api/v1/";
+import {Navigation} from './components'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Redirect } from 'react-router-dom';
+import {HomePage, NewsPage, TrackerPage} from './Pages';
+
 
 export class App extends Component {
-  state = {
-    quote : ""
-  }
-  getQuote = (symbol) => {
-    Axios.get(API_URL+`/quote?`, {params: {
-      symbol: symbol,
-      token: "c04d8tn48v6u76cjevm0"
-      }})
-      .then(response => {
-        console.log(response.data);
-        this.setState({quote:response.data})
-        console.log(this.state.quote);
-      })
-      .catch(error => console.log(error));
-  } 
   
   render() {
     return (
       <div>
-        <SearchForm submitHandler={this.getQuote}/>
+        <Router>
+          <Navigation />
+          <Switch>
+            <Route exact path="/" render={() => {
+              return (
+                <Redirect to="/home" />)}}
+            />
+            <Route
+              path="/home"
+              render={(props) => <HomePage {...props} />}
+            />
+            <Route
+              path="/news"
+              render={(props) => <NewsPage {...props} />}
+            />
+            <Route
+              path="/tracker"
+              render={(props) => <TrackerPage {...props} />}
+            />
+          </Switch>
+        </Router>
       </div>
     )
   }
---
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,31 @@
+import {SearchForm} from '../../components';
+import React, { Component } from 'react'
+import Axios from 'axios';
+const API_URL = "https://finnhub.io/api/v1/";
+
+export class HomePage extends Component {
+    state = {
+        quote : ""
+      }
+      getQuote = (symbol) => {
+        Axios.get(API_URL+`/quote?`, {params: {
+          symbol: symbol,
+          token: "c04d8tn48v6u76cjevm0"
+          }})
+          .then(response => {
+            console.log(response.data);
+            this.setState({quote:response.data})
+            console.log(this.state.quote);
+          })
+          .catch(error => console.log(error));
+      } 
+    render() {
+        return (
+            <div>
+                <SearchForm submitHandler={this.getQuote} />
+            </div>
+        )
+    }
+}
+
+export default HomePage
