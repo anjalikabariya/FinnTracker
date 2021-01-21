dd571e5299620087b2b5f2fee800e662097c090f
Anjali Kabariya <anjali.kabariya21@gmail.com>
2021-01-21 17:32:14 -0500

    chart component created

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -1,6 +1,8 @@
 import {SearchForm} from '../../components';
 import React, { Component } from 'react'
 import Axios from 'axios';
+import CanvasJS from "canvasjs-react-charts"
+
 const API_URL = "https://finnhub.io/api/v1/";
 
 export class HomePage extends Component {
@@ -8,9 +10,12 @@ export class HomePage extends Component {
         quote : ""
       }
       getQuote = (symbol) => {
-        Axios.get(API_URL+`/quote?`, {params: {
+        Axios.get(API_URL+`/stock/candle?`, {params: {
           symbol: symbol,
-          token: "c04d8tn48v6u76cjevm0"
+          token: "c04d8tn48v6u76cjevm0",
+          resolution: 15,
+          from: 1605543327,
+          to: 1605629727
           }})
           .then(response => {
             console.log(response.data);
@@ -23,6 +28,14 @@ export class HomePage extends Component {
         return (
             <div>
                 <SearchForm submitHandler={this.getQuote} />
+                <CanvasJsChart 
+                    options={{
+                        data:[{
+                            type:"candlestick",
+                            
+                        }]
+                    }}
+                />    
             </div>
         )
     }
