0fcce3a7fe53da7e5aa051749664b6f8568b3bd4
Anjali Kabariya <anjali.kabariya21@gmail.com>
2021-02-02 11:32:51 -0500

    added comments to components

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,6 @@ import {Navigation, Register, Login} from './components'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Redirect } from 'react-router-dom';
 import {HomePage, NewsPage, TrackerPage} from './Pages';
-// import firebase from './firebase';
-// firebase.firestore().collection('transactions').delete({
-//   id:1,
-//   type:"Sale",
-//   amount:100,
-//   stockName: "APPLE",
-//   date:new Date("20 Jan 2021")
-// })
 
 export class App extends Component {
   
@@ -23,14 +15,6 @@ export class App extends Component {
             <Route exact path="/" render={() => {
               return (
                 <Redirect to="/home" />)}}
-            />
-            <Route
-              path="/login"
-              render={(props) => <Login />}
-            />
-            <Route
-              path="/register"
-              render={(props) => <Register />}
             />
             <Route
               path="/home"
